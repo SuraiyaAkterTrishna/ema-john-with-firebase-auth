f78ded76e189977cd565ea99ee4fa312070fa6d6
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2023-12-18 13:15:49 +0600

    firebase initialization

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDzElO07z3xSI-LqdgE_KeehdA8CTwSVlA",
+  authDomain: "auth-master-9ac9e.firebaseapp.com",
+  projectId: "auth-master-9ac9e",
+  storageBucket: "auth-master-9ac9e.appspot.com",
+  messagingSenderId: "475719850658",
+  appId: "1:475719850658:web:ba6dbfc3d4d264cd8615fb"
+};
+
+const app = initializeApp(firebaseConfig);
+export default app;
